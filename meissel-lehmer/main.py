104af167c2578f158a441961785181256347fada
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2024-05-24 12:06:34 -0500

    chore: Refactor main.py for command line argument support and improve performance

--- meissel-lehmer/main.py
+++ meissel-lehmer/main.py
@@ -1,28 +1,12 @@
 from PrimeCount import PrimeCount
-from matplotlib import pyplot as plt
 import time
+import sys
 
 if __name__ == "__main__":
-    xs = [i for i in range(1, 50)]
-    # p2s = [PrimeCount(i).count() for i in xs]
-    # times = [i * 0.0001 for i in xs]
-
-    p2s = []
-    time_taken = []
-    for i in xs:
-        t1 = time.time()
-        pc = PrimeCount(i)
-        p2s.append(pc.count())
-        t2 = time.time()
-        time_taken.append(t2 - t1)
-    plt.plot(xs, p2s)
-    plt.xlabel("x")
-    plt.ylabel("P2(x)")
-    plt.title("P2(x) vs x")
-    plt.show()
-
-    plt.plot(xs, time_taken)
-    plt.xlabel("x")
-    plt.ylabel("Time taken")
-    plt.title("Time taken vs x")
-    plt.show()
+    num = int(sys.argv[1]) if len(sys.argv) > 1 else 50
+    pc = PrimeCount(num)
+    start = time.time()
+    pc.count()
+    end = time.time()
+    print(f"Time taken: {end-start} seconds")
+    print("The number of primes less than or equal to", num, "is", pc.count())
